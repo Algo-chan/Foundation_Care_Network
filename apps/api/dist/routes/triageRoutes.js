e281f954a610d52d118e40a57d4c22d01e752847
MRQ67 <kasaboody@gmail.com>
2026-05-19 15:46:00 +0300

    fix: the file struture of the project

--- apps/api/dist/routes/triageRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const triageController_1 = require("../controllers/triageController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-// Secure triage submission
-router.post('/analyze', auth_1.authenticate, triageController_1.submitTriage);
-exports.default = router;
